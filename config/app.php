32af751c70fc30ef67dc276e5e4e1bf8e585c253
Nerdy Gedoni <gedoniani@gmail.com>
2022-12-07 12:00:28 +0000

    app update

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laravel-production-7f03.up.railway.app'),
 
     'asset_url' => env('ASSET_URL', null),
 
