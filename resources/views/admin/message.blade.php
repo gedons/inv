049e50e2afb38c77cd2d4ad5079ab5dc82c10f7d
Nerdy Gedoni <gedoniani@gmail.com>
2022-12-08 14:27:37 +0000

    route fix

--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -230,7 +230,7 @@
                         <div class="card-header">{{ __('Send Message') }}</div>
                         
                         <div class="card-body">
-                            <form method="POST" action="{{route('message.send')}}">
+                            <form method="POST" action="admin/message/send">
                                 @csrf
         
                                 <div class="row mb-3">
