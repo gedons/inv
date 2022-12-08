440f359f940eabc5616d7c5cedfe780ac30c84ea
Nerdy Gedoni <gedoniani@gmail.com>
2022-12-08 14:41:07 +0000

    route message

--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -230,7 +230,7 @@
                         <div class="card-header">{{ __('Send Message') }}</div>
                         
                         <div class="card-body">
-                            <form method="POST" action="admin/message/send">
+                            <form method="POST" action="message/send">
                                 @csrf
         
                                 <div class="row mb-3">
