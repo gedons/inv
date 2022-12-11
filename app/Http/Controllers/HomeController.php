36ebba8be91ad95d8945a045689eb91a3aff59dc
Nerdy Gedoni <gedoniani@gmail.com>
2022-12-11 10:48:36 +0000

    message edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\Investment;
-use App\Events\Message;
+use App\Models\Message;
 
 class HomeController extends Controller
 {
