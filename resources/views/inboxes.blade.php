07fb4c4de3a07f5153e2a6c7e516ad0c5a5156d6
Nerdy Gedoni <gedoniani@gmail.com>
2022-12-11 12:28:42 +0000

    mess set

--- resources/views/inboxes.blade.php
+++ resources/views/inboxes.blade.php
@@ -68,7 +68,6 @@
         <a class="nav-link" href="{{route('front.inbox')}}">
             <i class="fas fa-fw fa-envelope"></i>
             <span>Message</span>
-            <i class="fas fa-bell fa-fw"></i>
             <span class="badge badge-danger badge-counter">3+</span>
         </a>
     </li>
@@ -84,6 +83,44 @@
 </ul>
 <!-- End of Sidebar -->
 </div>
+
+
+
+        <!-- Logout Modal-->
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="{{ route('admin.logout')}}">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('dash/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('dash/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('dash/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('dash/js/sb-admin-2.min.js')}}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{asset('dash/vendor/chart.js/Chart.min.js')}}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset('dash/js/demo/chart-area-demo.js')}}"></script>
+    <script src="{{asset('dash/js/demo/chart-pie-demo.js')}}"></script>
 
 </body>
 
