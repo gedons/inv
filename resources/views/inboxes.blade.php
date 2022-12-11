3f764f050b1705aba2c907d2fcaca7f52aa4a22b
Nerdy Gedoni <gedoniani@gmail.com>
2022-12-11 13:23:33 +0000

    card

--- resources/views/inboxes.blade.php
+++ resources/views/inboxes.blade.php
@@ -199,7 +199,19 @@
                     <h1 class="h3 mb-0 text-gray-800">Message</h1>
                 </div>
 
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Inbox</h6>
+                        </div>
 
+                        <div class="card-body">
+                            
+                        </div>
+                        </div>
+                    </div>
+                </div>
                 
 
 
