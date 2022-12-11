00eafc3d1085a1ab05e1863e235771e9fa18fecd
Nerdy Gedoni <gedoniani@gmail.com>
2022-12-11 12:02:02 +0000

    yy

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -10,31 +10,34 @@ use App\Notifications\MessageCreated;
 
 class FrontController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     
     public function inbox()
     {
-        $inboxes = Message::all();
-        return view('inboxes', compact('inboxes'));
+        return view('inboxes');
     }
 
-    public function sent(MessageRequest $request)
-    {
-        $validated = $request->validated();
+    // public function sent(MessageRequest $request)
+    // {
+    //     $validated = $request->validated();
 
-        $data = Message::create($validated);
+    //     $data = Message::create($validated);
 
-        //notify
-        // $user = User::first();
-        // $user->notify(new MessageCreated($inbox));
+    //     //notify
+    //     // $user = User::first();
+    //     // $user->notify(new MessageCreated($inbox));
 
-       // Notification::send(auth()->user(), new UserCreatePost($data['title']));
-        return Redirect()->back()->with('message', 'Message Sent Successfully!!!');
-    }
+    //    // Notification::send(auth()->user(), new UserCreatePost($data['title']));
+    //     return Redirect()->back()->with('message', 'Message Sent Successfully!!!');
+    // }
 
-    public function delete($id){
-        $message = Message::where('id',$id)->first();
-        $message->delete();
+    // public function delete($id){
+    //     $message = Message::where('id',$id)->first();
+    //     $message->delete();
 
-        return Redirect()->back()->with('message', 'Message Deleted Successfully!!!');
-    }
+    //     return Redirect()->back()->with('message', 'Message Deleted Successfully!!!');
+    // }
 }
