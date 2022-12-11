1061b30951977eb800f3b60805c0250323f9cbf0
Nerdy Gedoni <gedoniani@gmail.com>
2022-12-11 12:24:04 +0000

    tesd

--- resources/views/inboxes.blade.php
+++ resources/views/inboxes.blade.php
@@ -22,7 +22,70 @@
 
 </head>
 
-<body></body>
+<body id="page-top">
+
+        <!-- Page Wrapper -->
+        <div id="wrapper">
+
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('home')}}">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Johnny❤Jena Olsen</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Interface
+    </div>
+
+    <!-- Nav Item - Utilities Collapse Menu -->
+     <li class="nav-item">
+        <a class="nav-link" href="{{route('activities')}}">
+            <i class="fas fa-fw fa-flag"></i>
+            <span>Activities</span>
+        </a>
+    </li>
+
+       <li class="nav-item">
+        <a class="nav-link" href="{{route('front.inbox')}}">
+            <i class="fas fa-fw fa-envelope"></i>
+            <span>Message</span>
+            <i class="fas fa-bell fa-fw"></i>
+            <span class="badge badge-danger badge-counter">3+</span>
+        </a>
+    </li>
+
+       <li class="nav-item">
+        <a class="nav-link" href="{{route('logout')}}">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Logout</span>
+        </a>
+    </li>
+
+
+</ul>
+<!-- End of Sidebar -->
+</div>
+
+</body>
 
 
 </html>
